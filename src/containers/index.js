7a9a3d55332c4915b0922a6851974adbb4b53964
Johan Lövberg <Lovberg_93@hotmail.com>
2023-10-10 19:30:47 +0200

    Revert "Revert "Updates""
    This reverts commit e9d9cf85c687a31cfec676a03bca1d02a0113366.

--- src/containers/index.js
+++
@@ -1,8 +0,0 @@
-export { default as Blog } from './blog/Blog';
-export { default as Features } from './features/Features';
-export { default as Footer } from './footer/Footer';
-export { default as Header } from './header/Header';
-export { default as Possibility } from './possibility/Possibility';
-export { default as Service } from './Service/Service';
-export { default as About } from './About/About'
-export { default as BasicFooter } from './basicFooter/basicFooter';
